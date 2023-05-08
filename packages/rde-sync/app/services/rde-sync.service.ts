90bed67fe1088324fcd7218fee9552d959b7e096
sharleenawinja <awinjasharleen@gmail.com>
2023-05-08 15:34:15 +0300

    checking identifier

--- packages/rde-sync/app/services/rde-sync.service.ts
+++ packages/rde-sync/app/services/rde-sync.service.ts
@@ -30,14 +30,19 @@ class RdeSyncService {
 
     const handleRow = async (row: RowDataPacket) => {
       const patientId = row.patient_id;
+      const connection = await ETL_POOL.getConnection();
+
+      const checkingQuery = `SELECT 1 FROM rde_sync_queue WHERE patient_id = ${patientId} AND reporting_month = ${reportingMonth}`;
+      const [existingRows] = await connection.execute(checkingQuery);
+      console.log("existing", existingRows);
 
       const moment = require("moment-timezone");
       let now = moment().tz("Africa/Nairobi");
       let formattedDateTime = now.format("YYYY-MM-DD HH:mm:ss");
+      // create a function to check if identifiers already exist in rde table, if they exist return a response saying they already exist and which reporting month they are in; check if patient already exists in that reporting month that is being passed
 
       let query = `INSERT INTO rde_sync_queue (user_id, patient_id, date_created, reporting_month, status) VALUES ('${userId}', ${patientId}, '${formattedDateTime}', '${reportingMonth}', 'QUEUED')`;
 
-      const connection = await ETL_POOL.getConnection();
       const [rows] = await connection.execute(query);
       const { affectedRows } = rows as AffectedRows;
       connection.release();
@@ -123,10 +128,15 @@ class RdeSyncService {
     const query = `REPLACE INTO etl.hiv_monthly_report_dataset_frozen (SELECT * from etl.hiv_monthly_report_dataset_v1_2
           WHERE hiv_monthly_report_dataset_v1_2.endDate = '${reportingMonth}' AND hiv_monthly_report_dataset_v1_2.person_id
           IN (${whereQuery.slice(0, -1)}));`;
+    // run this query with details of a patient whose data has already been frozen
+    // idea is to optimize code and ensure that update status function is not called when zero rows are affected
 
     try {
       const connection = await ETL_POOL.getConnection();
       const [rows] = await connection.execute(query);
+      // check if previous query execution was sucessful before updating status  //prevent adding duplicate patients for the same month in rde
+      // ALTER TABLE rde_sync_queue ADD UNIQUE unique_patient_reporting UNIQUE (patient_id, reporting_month);
+      await this.updatePatientStatus(patientIds, QueueStatus.FROZEN);
       connection.release();
       return h.response(rows).code(201);
     } catch (e) {
